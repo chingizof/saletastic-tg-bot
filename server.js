41ee0439ad7163d27d2bcda235329785e8674fbc
Mister-718 <59221853+Mister-718@users.noreply.github.com>
2021-08-02 16:24:44 +0600

    added createNewDialog function

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const token = '1903938146:AAG_ClwVuiXRYCG0s5MEVKTjWDmfuJwNSmw'
 const express = require('express')
 const path = require('path')
 const port = process.env.PORT || 8080
+const mongoose = require('mongoose');
 
 const bot = new TelegramBot(token, { polling: true })
 const app = express()
@@ -11,6 +12,12 @@ bot.onText(/main/, (msg, match) => {
 	const chatId = msg.chat.id
 	bot.sendMessage(chatId, `Hello! Are you here to receive a discount for Banarasi Outfits ?\n1. Yes\n2. No`)
 })
+
+const mongoDB = 'mongodb+srv://nurlan:qweQWE123@cluster0.ikiuf.mongodb.net/tgdb?retryWrites=true&w=majority';
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 app.listen(port, function listen() {
     console.log(`Server is listening at http://localhost:${port}`);
